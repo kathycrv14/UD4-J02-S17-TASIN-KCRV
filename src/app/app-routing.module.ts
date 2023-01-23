8d210c1408d29027c9608d6e9e3d16039034c033
kathycrv14 <katherinecristinarv@outlook.com>
2023-01-23 02:11:08 -0500

    Mensaje de commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarComponent } from './principal/editar/editar.component';
+import { TablaComponent } from './principal/tabla/tabla.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tabla', component: TablaComponent },
+  { path: 'editar', component: EditarComponent },
+  { path: 'productos/:producto', component: EditarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
